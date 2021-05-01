8c4db7e90c3b494f2a578f2542698959f5f65b70
Evgeniy Khomutinnikov <evgeniy.khomutinnikov@iva-tech.ru>
2021-05-01 16:23:51 +0300

    pubspec changed
    Comments removed.
    res folder for assets added.
    flutter_svg added.
    Roboto font added.
    StatelessWidget created.

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        fontFamily: 'Roboto',
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Places'),
     );
   }
 }
@@ -108,6 +100,19 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MyFirstWidget extends StatelessWidget {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    print('build called ${++counter}');
+    return Container(
+      child: Center(
+        child: Text('Hello!'),
+      ),
     );
   }
 }
