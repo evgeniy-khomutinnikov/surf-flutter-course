2f685555ab1d59307eb0ba5ac47ce6b51ebb1c54
Evgeniy Khomutinnikov <khomin.e@mail.ru>
2021-05-21 17:48:39 +0300

    Добавлены моковые данные достопримечательностей

---
+++ lib/ui/domain/sight.dart
@@ -0,0 +1,17 @@
+class Sight {
+  String name;
+  double lat;
+  double lon;
+  String url;
+  String details;
+  String type;
+
+  Sight(
+    this.name,
+    this.lat,
+    this.lon,
+    this.url,
+    this.details,
+    this.type,
+  );
+}
