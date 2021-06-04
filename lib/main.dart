8e9fa1bbffb8102490d21b3196dc3c759b9a4b93
Evgeniy Khomutinnikov <khomin.e@mail.ru>
2021-06-04 14:53:18 +0300

    Реализованы TabBar и BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:places/ui/screen/sight_list_screen.dart';
+import 'package:places/ui/screen/visiting_screen.dart';
 
 void main() => runApp(App());
 
@@ -15,7 +16,7 @@ class App extends StatelessWidget {
         backgroundColor: Colors.white,
         primarySwatch: Colors.blue,
       ),
-      home: SightListScreen(),
+      home: VisitingScreen(),
     );
   }
 }
